b99f817992527693fd6d4ee9ccd32345304c423e
leeraper909 <lraper@linkupcomms.com>
2025-08-04 20:26:23 +0100

    Update defcon.js

--- js/defcon.js
+++ js/defcon.js
@@ -1,9 +1,1 @@
 
-document.addEventListener('DOMContentLoaded', () => {
-  let level = 4;
-  const el = document.getElementById('defconLevel');
-  setInterval(() => {
-    level = (level === 1) ? 5 : level - 1;
-    el.textContent = level;
-  }, 3000);
-});
