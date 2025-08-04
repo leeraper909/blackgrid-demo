563039204da874bdac1353c9fb0afeed68598d06
leeraper909 <lraper@linkupcomms.com>
2025-08-04 20:30:41 +0100

    Add files via upload

--- js/defcon.js
+++ js/defcon.js
@@ -1,1 +1,9 @@
 
+document.addEventListener('DOMContentLoaded', () => {
+  let level = 3;
+  const el = document.getElementById('defconLevel');
+  setInterval(() => {
+    level = (level === 1) ? 5 : level - 1;
+    el.textContent = level;
+  }, 3000);
+});
