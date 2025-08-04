0b3033e64b1070f51678a215dcc8440fdd2b18c1
leeraper909 <lraper@linkupcomms.com>
2025-08-04 20:27:06 +0100

    Update lockmode.js

--- js/lockmode.js
+++ js/lockmode.js
@@ -1,7 +1,1 @@
 
-document.addEventListener('DOMContentLoaded', () => {
-  const button = document.getElementById('modeToggle');
-  button.addEventListener('click', () => {
-    document.body.classList.toggle('locked');
-  });
-});
