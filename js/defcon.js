b2baffa475d6d847454701f235c40176a803e9f9
leeraper909 <lraper@linkupcomms.com>
2025-08-05 11:05:23 +0100

    Delete js directory

--- js/defcon.js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', () => {
-  let level = 3;
-  const el = document.getElementById('defconLevel');
-  setInterval(() => {
-    level = (level === 1) ? 5 : level - 1;
-    el.textContent = level;
-  }, 3000);
-});
--- js/lockmode.js
+++
@@ -1,7 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', () => {
-  const button = document.getElementById('modeToggle');
-  button.addEventListener('click', () => {
-    document.body.classList.toggle('locked');
-  });
-});
